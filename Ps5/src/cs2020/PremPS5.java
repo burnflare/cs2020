9e302fad10ecfe4e3b157eb435e2a77c97d82dcd
Vishnu Prem <vishnu@vishnuprem.com>
2014-03-20 18:00:19 +0800

    interim

--- Ps5/src/cs2020/PremPS5.java
+++ Ps5/src/cs2020/PremPS5.java
@@ -6,9 +6,13 @@ package cs2020;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+
+
+class Pair {
+	public HashSet<String> hs = new HashSet<String>(); public int count = 0;
+}
 
 /**
  * @author vishnu
@@ -24,51 +28,52 @@ public class PremPS5 {
 	}
 
 	public static void main(String[] args) {
-		StopWatch sw = new StopWatch();
-		sw.start();
-		HashMap<Integer, Pair> hm = new HashMap<Integer, Pair>();
-		try {
-			FileReader dataFile = new FileReader(args[0]);
-	        BufferedReader bufferedDataFile = new BufferedReader(dataFile);
-	        bufferedDataFile.readLine(); // discarding size
-	        
-	        String line;
-	        
-	        while ((line = bufferedDataFile.readLine()) != null) {
-	        	String sLine = stringSort(line);
+		for(String s : args) {
+			StopWatch sw = new StopWatch();
+			sw.start();
+			HashMap<Integer, Pair> hm = new HashMap<Integer, Pair>(500);
+			try {
+				FileReader dataFile = new FileReader(s);
+		        BufferedReader bufferedDataFile = new BufferedReader(dataFile);
+		        bufferedDataFile.readLine(); // Dont care about size, discarding
+		        
+		        String line;
+		        int length;
 	        	Pair p;
-	        	p = hm.get(sLine.length());
-	        	if(p==null) {
-	        		p = new Pair(new HashSet<String>(), 0);
-	        		hm.put(sLine.length(), p);
-	        	}
 	        	
-	        	if(p.hs.contains(sLine)) p.count++;
-	        	else p.hs.add(sLine);
+	        	int hmcount = 0;
+		        while ((line = stringSort(bufferedDataFile.readLine())) != null) {
+		        	length = line.length();
+		        	p = hm.get(length);
+		        	if(p==null) {
+		        		p = new Pair();
+		        		p.hs.add(line);
+		        		hm.put(length, p);
+		        		hmcount++;
+		        	} else {
+			        	if(p.hs.contains(line)) { p.count++; }
+			        	else p.hs.add(line);
+		        	}
+				}
+		        
+		        int count = 0;
+		        for(Pair pp : hm.values()) {
+		        	if(pp.count==0) continue;
+		        	System.out.println(pp.count);
+		        	count += (pp.count*(pp.count+1))/2;
+		        }
+		        sw.stop();
+		        System.out.println(count + " " + sw.getTime() +" " + hmcount);
+			} catch (Exception e) {
+				System.out.println(e);
 			}
-	        int count = 0;
-	        for(Pair p : hm.values()) {
-	        	if(p.count==0) continue;
-	        	count += (p.count*(p.count+1))/2;
-	        }
-	        sw.stop();
-	        System.out.println(count + " " + sw.getTime());
-		} catch (Exception e) {
-			System.out.println(e);
 		}
 	}
 	
 	private static String stringSort(String str) {
+		if(str==null) return null;
 		char[] a = str.toCharArray();
 		Arrays.sort(a);
 		return new String(a);
-	}
-}
-
-class Pair {
-	HashSet<String> hs; int count;
-	public Pair(HashSet<String> hs, int count) {
-		this.hs = hs;
-		this.count = count;
 	}
 }
